1048abb8057b97f853207d1f30ecfc72258fc89d
LANNDS18 <lannds18@icloud.com>
2022-05-10 05:43:17 +0100

    adding script

--- mpi.c
+++ mpi.c
@@ -44,9 +44,9 @@ void Get_input(int argc, char *argv[]){
 int main(int argc, char *argv[])  {
 
     double start, finish, loc_elapsed, elapsed, maxCommTime, maxCompTime;
-    start = MPI_Wtime();
 
     MPI_Init(NULL, NULL);
+    start = MPI_Wtime();
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     Get_input(argc, argv);
